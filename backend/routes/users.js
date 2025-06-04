6a99b31056e115b14ef04abab51ab29c754c3da5
sky454656 <kyseo2477@gmail.com>
2025-06-05 00:15:53 +0900

    password 암호화

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+const bcrypt = require('bcryptjs');
+
 router.post('/register', async (req, res) => {
   try{
-    const {username} = req.body;
+    const {username, password} = req.body;
 
     const existing = await User.findOne({username});
     if (existing)
       return res.status(409).json({message: 'Username already exists.'});
 
-      const newUser = new User(req.body);
-      await newUser.save();
-      res.status(201).json({message : 'register success'});
+    const hashedpassword = await bcrypt.hash(password, 10)
+    const newUser = new User({username, password : hashedpassword});
+
+    await newUser.save();
+    res.status(201).json({message : 'register success'});
     }catch (err){
       res.status(400).json({ message: 'register failure', error: err });
     }
@@ -22,9 +26,15 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
 
-    const { username, password } = req.body;
+    const { username , password } = req.body;
+
     const user = await User.findOne({ username });
-    if (!user || user.password !== password) {
+    if (!user)
+      return res.status(401).json({ message: 'login failure' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch){
       return res.status(401).json({ message: 'login failure' });
     }
     res.json({ message: 'login success', username});
